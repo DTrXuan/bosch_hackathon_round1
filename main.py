2fdb7b3d373b4d433a7207bb273db30099cab434
Doan Truong Xuan (MS/EJV23-PS) <Xuan.DoanTruong@vn.bosch.com>
2022-07-14 22:40:59 +0700

    finish

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ ult_sen = UltrasonicSensor(Port.S1)
 pressed = [] #mang luu gia tri nut nhan
 button = 0 #bien luu gia tri nut nhan
 
-VEL_MAX = 1050 #dps Degree per second (DPS)
-VEL_MIN_CANMOVE = 10 #dps toc do nho nhat co the lam di chuyen xe
+VEL_MAX = 100 #duty 
+VEL_MIN_CANMOVE = 15 #duty
 VEL_STOP = 0 #stop 
 
 DISOFFSET = 0 #mm bu so sai lech khoang cach thuc te voi ket qua do duoc
-STOP_DIST = 45 #mm khoang cach phai dung lai
+STOP_DIST = 50 #mm khoang cach phai dung lai
 REDUCE_VEL_ZONE = 150 #mm vung giam toc: khoang cach tu xe den vat can. trong vung nay xe bat dau giam toc do
+SMOOTH_CALIB = 90
 DIS_MAX = 2550 #mm k co vat can hoac vat can qua gan truoc cam bien.
 
 """
@@ -52,11 +53,13 @@ VELOFFSET = VEL_MAX - VELFACTOR*REDUCE_VEL_ZONE
 # raw_dist = ult_sen.distance()
 raw_dist = DIS_MAX #khoảng cách cảm biến đọc được
 calculated_dist = raw_dist + DISOFFSET #khoảng cách sau khi bù sai số
-
+calculated_dist_old = calculated_dist
 vel = 0 #biến vận tốc hiện tại
 prevel = vel #biến vận tốc trước đó
 able2go_b = False
 demand2go_b = False
+safe_check_b = False
+
 
 def veloc_calc(dist):
     vel_rt = 0
@@ -72,20 +75,9 @@ def veloc_calc(dist):
     # chặn trên/dưới cho tốc độ
     if (vel_rt > VEL_MAX): 
         vel_rt = VEL_MAX
-    if (vel_rt < VEL_MIN_CANMOVE): 
+    if (vel_rt <= VEL_MIN_CANMOVE): 
         vel_rt = VEL_STOP
     return vel_rt
-
-def wait_for_driver():
-    ev3.speaker.say("DANGER! PLEASE PRESS BRAKE")        
-    while (not Button.DOWN in ev3.buttons.pressed()):
-        for i in range(3):
-            ev3.speaker.beep(frequency=500, duration=100)
-        ev3.light.on(Color.RED)
-        wait(20)
-        ev3.light.off()
-        
-    ev3.speaker.say("OK")
 
 #
 """ MAIN """
@@ -109,13 +101,27 @@ if (calculated_dist >= DIS_MAX or calculated_dist <= STOP_DIST):
 else:
     able2go_b = True
     
-ev3.speaker.say("GEC GOOOO")
+ev3.speaker.say("GEC GO")
+
+array_len = 5
+array = [0]*array_len
+peek = 0
+
+for i in range(array_len):
+    array[i] = ult_sen.distance()
 
 while True:
-
+    
     # doc khoang cach
-    raw_dist = ult_sen.distance()
+    peek = peek + 1
+    peek = peek % array_len
+    array[peek] = ult_sen.distance()
+    for i in array:
+        raw_dist += i 
+    raw_dist = int(raw_dist/array_len)
+    
     calculated_dist = raw_dist + DISOFFSET
+    
     ev3.screen.print("dist: "+ str(calculated_dist) + "vel: " + str(vel))
     
     # kiem tra dieu kien nguy hiem
@@ -128,23 +134,37 @@ while True:
     if (demand2go_b == True):
         # dieu khien xe
         if (able2go_b == True):
-            vel = veloc_calc(calculated_dist)
-                        
+            
+            if (calculated_dist <= REDUCE_VEL_ZONE - SMOOTH_CALIB):
+                if (calculated_dist_old != calculated_dist):
+                    vel = veloc_calc(calculated_dist)
+            else:
+                vel = veloc_calc(calculated_dist)
+
+            calculated_dist_old = calculated_dist
+        drv_mot.dc(vel) #comment for mo phong
+                    
         # yeu cau nhan phanh khi gap nguy hiem
         if (able2go_b == False):
-            # robot.straight(0)
+            ev3.light.on(Color.RED)
+            safe_check_b = False
             vel = VEL_STOP
-            # wait_for_driver()
             demand2go_b = False
-        
-        drv_mot.run(vel) #comment for mo phong
-    
+            
     # Chờ lệnh từ driver
     if (demand2go_b == False):
-        ev3.light.on(Color.RED)
+        
+        # drv_mot.brake()
+        drv_mot.run(0)
+        
+        if ((Button.DOWN in ev3.buttons.pressed()) and (able2go_b == True)):
+            safe_check_b = True
+            ev3.light.on(Color.YELLOW)
         if ((Button.UP in ev3.buttons.pressed()) and (able2go_b == True)):
-            ev3.speaker.say("OK GO")
-            demand2go_b = True
+            if (safe_check_b == True):
+                ev3.light.on(Color.GREEN)
+                ev3.speaker.say("OK GO")
+                demand2go_b = True
         elif ((Button.UP in ev3.buttons.pressed()) and (able2go_b == False)):
             ev3.speaker.say("DANGER CAN NOT GO FORWARD")
             demand2go_b = False
